ee735ac4c13c8c80f394be66011653431f140eee
Brian Matzelle <brianmatzelle@Brians-MacBook-Air.local>
2022-10-25 15:26:21 -0400

    parsing string now

--- src/training_data.rs
+++ src/training_data.rs
@@ -11,6 +11,8 @@ impl TrainingData {
         let contents = fs::read_to_string(path)
             .expect("Should have been able to read the file");
         
-        println!("With text:\n{contents}");
+        let mut lines = contents.lines();
+        // println!("line[0]: {}", lines.nth(0).unwrap());
+        topology.push(lines.nth(0).expect("Topology instruction should be given here"));
     }
 }
